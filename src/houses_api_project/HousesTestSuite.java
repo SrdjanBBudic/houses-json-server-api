7b34bcdd255ac68e0c4d5ae5e9e1371c74ae7661
SrdjanBBudic <78545430+SrdjanBBudic@users.noreply.github.com>
2021-02-07 20:37:34 +0100

    3 Tests

--- src/houses_api_project/HousesTestSuite.java
+++ src/houses_api_project/HousesTestSuite.java
@@ -1,41 +1,118 @@
 package houses_api_project;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static io.restassured.RestAssured.given;
+
 import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
 
 public class HousesTestSuite {
+
+	@BeforeEach
+    public void setUp() {
+        RestAssured.baseURI = "http://localhost:3000";
+    }
+	
 	@Test
-	public void PropertiesPositiveTests()
-	{
-		RestAssured.baseURI= "http://localhost:3000";
-		//Base URI of the json server
+	public void housesCheckCityAndPriceRange() {
 		
-		String getPropertyType = given().log().all().queryParam("price_gte", "200000").queryParam("price_lte", "900000").queryParam("city", "Austin")
-				.when().get("houses")
-				.then().assertThat().extract().response().asString();
-		// Populating a String "getPropertyType" with objects that fulfill the needed parameters
+		String housesRaw = given()
+	               .queryParam("price_gte", "200000")
+	               .queryParam("price_lte", "900000")
+	               .queryParam("city", "Austin")
+	               .when().get("houses")
+	               .then().assertThat().statusCode(200).extract().response().asString();
 		
-        try {
-        	
-            JSONArray propertyTypeArray = new JSONArray(getPropertyType);
-            
-            for (int i = 0; i < propertyTypeArray.length(); i++)
-            {
-                JSONObject propertyTypeObjectForParsing = propertyTypeArray.getJSONObject(i);
-                String propertyCityName = propertyTypeObjectForParsing.getString("city");
-                assertEquals("Austin", propertyCityName);
-                int propertyPriceValue = propertyTypeObjectForParsing.getInt("price");
-                assertTrue(200000 <= propertyPriceValue && propertyPriceValue <= 900000);
-            }
-        }
-        catch (Exception e) {
-        	System.out.println("Property price is out of range");
-        }
+		
+		JSONArray houses = rawStringToJsonArray(housesRaw);
+
+		
+		
+		for(int i = 0; i < houses.length(); i++) {
+			JSONObject house = houses.getJSONObject(i);
+			
+			assertEquals("Austin", house.get("city"));
+			assertTrue(200000 <=  house.getInt("price") &&  house.getInt("price") <= 900000);
+		}
 	}
+	
+	
+	@Test
+	public void housesCheckIfAllKeysArePresent() {
+		
+		String housesRaw = given()
+	               .queryParam("price_gte", "200000")
+	               .queryParam("price_lte", "900000")
+	               .queryParam("city", "Austin")
+	               .when().get("houses")
+	               .then().extract().response().asString();
+		
+		
+		JSONArray houses = rawStringToJsonArray(housesRaw);
+
+	
+		for(int i = 0; i < houses.length(); i++) {
+			JSONObject house = houses.getJSONObject(i);
+			house.get("id");
+			house.get("mls_id");
+			house.get("mls_listing_id");
+			house.get("property_type");
+			house.get("formatted_address");
+			house.get("address");
+			house.get("zip");
+			house.get("city");
+			house.get("state");
+			house.get("location");
+			house.get("bedrooms");
+			house.get("bathrooms");
+			house.get("list_date");
+			house.get("mls_update_date");
+			house.get("price_display");
+			house.get("price");
+			house.get("square_feet");
+			
+			
+		}
+	}
+	
+	
+	@Test
+	public void housesCheckIfArrayIsEmptyWhenParametersCantReadAnything() {
+		
+		String housesRaw = given()
+	               .queryParam("price_gte", "200000")
+	               .queryParam("price_lte", "210000")
+	               .queryParam("city", "Austin")
+	               .when().get("houses")
+	               .then().extract().response().asString();
+		
+		
+		JSONArray houses = rawStringToJsonArray(housesRaw);
+		assertEquals(houses.length(), 0);
+	
+	}
+	
+	
+	
+	private static JSONArray rawStringToJsonArray(String input) {
+        JSONArray result = null;
+        try {
+            result = new JSONArray(input);
+        }
+        catch (JSONException e) {
+            System.err.println(e);
+        }
+
+        return  result;
+    }
+	
+	
+	
 }
